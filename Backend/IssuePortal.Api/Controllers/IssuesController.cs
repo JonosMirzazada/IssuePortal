10a56fffb27306d3855a9ac869a9e7a73a988edb
Jonos Mirzazada <mirzazadajonos@yahoo.com>
2026-08-26 22:42:07 +0200

    changed the code in Issueportal.API & added the Models and Servies files

--- Backend/IssuePortal.Api/Controllers/IssuesController.cs
+++ Backend/IssuePortal.Api/Controllers/IssuesController.cs
@@ -1,3 +1,4 @@
+using IssuePortal.Api.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IssuePortal.Api.Controllers;
@@ -11,20 +12,64 @@ public class IssuesController : ControllerBase
     {
         return Ok(new[]
         {
-            new
+            new Issue
             {
                 Id = 1,
                 Title = "Login fungerar inte",
+                Description = "Användaren kan inte logga in.",
                 Status = "Open",
                 Priority = "High"
             },
-            new
+            new Issue
             {
                 Id = 2,
                 Title = "Dashboard behöver fixas",
+                Description = "Dashboard laddar inte korrekt.",
                 Status = "In Progress",
                 Priority = "Medium"
             }
         });
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetIssueById(int id)
+    {
+        if (id == 1)
+        {
+            return Ok(new Issue
+            {
+                Id = 1,
+                Title = "Login fungerar inte",
+                Description = "Användaren kan inte logga in.",
+                Status = "Open",
+                Priority = "High"
+            });
+        }
+
+        if (id == 2)
+        {
+            return Ok(new Issue
+            {
+                Id = 2,
+                Title = "Dashboard behöver fixas",
+                Description = "Dashboard laddar inte korrekt.",
+                Status = "In Progress",
+                Priority = "Medium"
+            });
+        }
+
+        return NotFound();
+    }
+
+    [HttpPost]
+    public IActionResult CreateIssue(Issue issue)
+    {
+        issue.Id = 3;
+
+        return CreatedAtAction(
+            nameof(GetIssueById),
+            new { id = issue.Id },
+            issue
+        );
     }
 }
---
+++ Backend/IssuePortal.Api/Models/Issue.cs
@@ -0,0 +1,14 @@
+namespace IssuePortal.Api.Models;
+
+public class Issue
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+
+    public string Status { get; set; } = "Open";
+
+    public string Priority { get; set; } = "Medium";
+}
