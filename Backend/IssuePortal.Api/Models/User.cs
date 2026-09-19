60aedfc09ec13da4a72259d05d3f65e9c37b5ba0
Jonos Mirzazada <mirzazadajonos@yahoo.com>
2026-09-19 15:40:23 +0200

    added some properties in user.cs.

--- Backend/IssuePortal.Api/Models/User.cs
+++ Backend/IssuePortal.Api/Models/User.cs
@@ -3,6 +3,8 @@ namespace IssuePortal.Api.Models;
 public class User
 {
     public int Id { get; set; }
+    public string PasswordHash { get; set; } = string.Empty;
+    public string Role { get; set; } = "User";
 
     public string Name { get; set; } = string.Empty;
 
